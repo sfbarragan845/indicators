6e3c5f5b78138308e4accfbe256cfab878ce60d6
sfbarragan845 <stevenfernando-22@hotmail.com>
2023-08-23 13:14:06 -0500

    add auth

---
+++ components/Menu.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="/actividad/:activities">Actividad</b-navbar-brand>
+      <b-navbar-brand href="#">Indicador</b-navbar-brand>
+      <b-navbar-brand href="/servicio">Servicio</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <v-spacer />
+            <div v-if="$auth.loggedIn">
+              {{$auth.user.email}}
+              <!-- username
+              logout button -->
+            </div>
+            <div v-else>
+              <!-- login
+              registration -->
+              <v-btn text to="/public/loging/loging">Login</v-btn>
+              <v-btn text to="/public/loging/loging">Register</v-btn>
+            </div>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtMenu',
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Menu/>
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+
+</script>
