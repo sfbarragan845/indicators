0d941f2ce5d969a5e4d1f22daea5bcd0292cf605
Ronald Ramírez <reramirez1@espe.edu.ec>
2023-08-28 08:32:47 -0500

    formularios

---
+++ components/Evidencia.vue
@@ -0,0 +1,341 @@
+
+
+<template>
+    <div class="mx-auto">
+        <div class="-mx-4 sm:-mx-8 px-4 py-2 sm:px-8 flex flex-wrap flex-grow justify-between">
+            <div class="grid place-items-center relative">
+                <span class="h-full absolute inset-y-0 left-0 flex items-center pl-3">
+                    <SearchIcon />
+                </span>
+                <input
+                    class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-5 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                    id="inline-searcg" type="text" placeholder="Search">
+            </div>
+            <div class="flex items-center py-2">
+                <button @click="showModal = true"
+                    class="inline-flex align-items-center gap-x-2 px-4 py-2 text-sm leading-5 font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-600">
+                    <AddIcon /> <span>Nuevo</span>
+                </button>
+            </div>
+
+            <!-- Modal  NUEVO -->
+            <transition enter-active-class="transition ease-out duration-300"
+                leave-active-class="transition ease-in duration-200">
+                <div v-if="showModal" class="fixed inset-0 flex items-center justify-center z-50">
+                    <div class="fixed inset-0 transition-opacity" @click="showModal = false">
+                        <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+                    </div>
+                    <div
+                        class="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full sm:max-w-lg">
+                        <!-- Contenido del modal -->
+                        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                            <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-black">Añada una nueva Evidencia</h3>
+
+                            <form @submit.prevent="agregarMeta">
+                                <div class="form-group">
+                                    <label for="fecha" class="form-label">Fecha</label>
+                                    <input v-model="nuevaEvidencia.fecha" type="date" id="fecha"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 light:bg-gray-600 light:border-gray-500 light:placeholder-gray-400 light:text-white"
+                                        placeholder="" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="resultadoObtenido" class="form-label">Resultados Obtenido</label>
+                                    <input v-model="nuevaEvidencia.resultadoObtenido" type="number" id="resultadoObtenido"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 light:bg-gray-600 light:border-gray-500 light:placeholder-gray-400 light:text-white"
+                                        placeholder="Resultado" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="linkActaEvidencia" class="form-label">link Acta Evidencia</label>
+                                    <input v-model="nuevaEvidencia.linkActaEvidencia" type="text" id="linkActaEvidencia"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 light:bg-gray-600 light:border-gray-500 light:placeholder-gray-400 light:text-white"
+                                        placeholder="Link de acta" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="metaId" class="form-label">Meta id</label>
+                                    <input v-model="nuevaEvidencia.metaId" type="number" id="metaId"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 light:bg-gray-600 light:border-gray-500 light:placeholder-gray-400 light:text-white"
+                                        placeholder="Escriba el id de la Meta" required>
+                                </div>
+                                <!-- Agrega aquí más campos si los necesitas -->
+                                <div class="flex justify-between mt-4">
+                                    <button type="button" @click="showModal = false"
+                                        class="btn btn-secondary">Cancelar</button>
+                                    <button type="submit" class="btn btn-primary">Enviar</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </transition>
+
+            <!-- Modal  EDITAR -->
+            <transition enter-active-class="transition ease-out duration-300"
+                leave-active-class="transition ease-in duration-200">
+                <div v-if="showModal2" class="fixed inset-0 flex items-center justify-center z-50">
+                    <div class="fixed inset-0 transition-opacity" @click="showModal2 = false">
+                        <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+                    </div>
+                    <div
+                        class="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full sm:max-w-lg">
+                        <!-- Contenido del modal -->
+                        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                            <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-black">Editar Evidencia</h3>
+
+                            <form @submit.prevent="actualizarMeta">
+                                <div class="form-group">
+                                    <label for="fecha" class="form-label">Fecha</label>
+                                    <input v-model="editarEvidencia.fecha" type="date" id="fecha"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 light:bg-gray-600 light:border-gray-500 light:placeholder-gray-400 light:text-white"
+                                        placeholder="" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="resultadoObtenido" class="form-label">Resultados Obtenido</label>
+                                    <input v-model="editarEvidencia.resultadoObtenido" type="number" id="resultadoObtenido"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 light:bg-gray-600 light:border-gray-500 light:placeholder-gray-400 light:text-white"
+                                        placeholder="Resultado" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="linkActaEvidencia" class="form-label">link Acta Evidencia</label>
+                                    <input v-model="editarEvidencia.linkActaEvidencia" type="text" id="linkActaEvidencia"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 light:bg-gray-600 light:border-gray-500 light:placeholder-gray-400 light:text-white"
+                                        placeholder="Link de acta" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="metaId" class="form-label">Meta id</label>
+                                    <input v-model="editarEvidencia.metaId" type="number" id="metaId"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 light:bg-gray-600 light:border-gray-500 light:placeholder-gray-400 light:text-white"
+                                        placeholder="Escriba el id de la Meta" required>
+                                </div>
+                                <!-- Agrega aquí más campos si los necesitas -->
+                                <div class="flex justify-between mt-4">
+                                    <button type="button" @click="showModal2 = false"
+                                        class="btn btn-secondary">Cancelar</button>
+                                    <button type="submit" class="btn btn-primary">Actualizar</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </transition>
+
+        </div>
+        <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 overflow-x-auto">
+            <div class="inline-block min-w-full shadow rounded-xl overflow-hidden">
+                <table class="min-w-full leading-normal">
+                    <thead>
+                        <tr class="bg-gray-50 border-b border-gray-200 text-xs leading-4 text-gray-500 uppercase">
+                            <th class="px-6 py-3 font-medium">Nro</th>
+                            <th class="px-6 py-3 font-medium">Fecha</th>
+                            <th class="px-6 py-3 font-medium">Resultado Obtenido</th>
+                            <th class="px-6 py-3 font-medium">Link Acta Evidencia</th>
+                            <th class="px-6 py-3 font-medium">Meta Id</th>
+                            <th class="px-6 py-3 text-center font-medium">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr :class="{ 'bg-gray-100': index % 2 === 1 }" v-for="(item, index) in itemsForCurrentPage" :key="item.id">
+                            <td class="px-6 py-2 border-b border-gray-200 text-sm">{{ item.id }}</td>
+                            <td class="px-6 py-2 whitespace-no-wrap border-b border-gray-200 text-sm">{{ item.fecha}}</td>
+                            <td class="px-6 py-2 whitespace-no-wrap border-b border-gray-200 text-sm">{{ item.resultadoObtenido}}</td>
+                            <td class="px-6 py-2 whitespace-no-wrap border-b border-gray-200 text-sm">{{ item.linkActaEvidencia}}</td>
+                            <td class="px-6 py-2 whitespace-no-wrap border-b border-gray-200 text-sm">{{ item.metaId}}</td>
+                            <td class="px-6 inline-flex w-full justify-center gap-x-4 py-2 whitespace-no-wrap border-b border-gray-200 text-sm text-center">
+                                <button @click="openEditModal(item)"
+                                    class="inline-block px-4 py-2 text-sm leading-5 font-medium rounded-md text-white bg-amber-500 hover:bg-amber-600">
+                                    <EditIcon />
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div
+                    class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between          ">
+                    <span class="text-xs xs:text-sm text-gray-900">
+                        Showing {{ (currentPage - 1) * 5 + 1 }} to {{ Math.min(currentPage * 5, items.length) }} of {{
+                            items.length }} Entries
+                    </span>
+                    <div class="inline-flex mt-2 xs:mt-0">
+                        <button @click="currentPage--" :disabled="currentPage === 1"
+                            class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l">
+                            Prev
+                        </button>
+                        <button @click="currentPage++" :disabled="currentPage === totalPages()"
+                            class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-r">
+                            Next
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+    </div>
+</template>
+
+<script>
+
+
+import icons from '~/icons';
+
+export default {
+
+    data() {
+        return {
+            currentPage: 1, // Cambia el valor inicial según tus necesidades
+
+            // ... tus otras variables de datos ...
+            showModal: false, // Variable para controlar la visibilidad del modal
+            showModal2: false, // Variable para controlar la visibilidad del modal
+            nuevaEvidencia: {
+                fecha: '',
+                resultadoObtenido: '',
+                linkActaEvidencia: '',
+                metaId: ''
+                // Agrega aquí más propiedades si las necesitas
+            },
+            editarEvidencia: {
+                fecha: '',
+                resultadoObtenido: '',
+                linkActaEvidencia: '',
+                metaId: ''
+                // Agrega aquí más propiedades si las necesitas
+            },
+        };
+    },
+    components: {
+        SearchIcon: icons.search,
+        AddIcon: icons.add,
+        EditIcon: icons.edit,
+        DeleteIcon: icons.delete,
+        ReturnIcon: icons.return
+    },
+    props: {
+        items: Array,
+
+    },
+
+
+
+    methods: {
+        agregarMeta() {
+            // Preparar los datos del nuevo indicador
+            const nuevaEvidenciaData = {
+                fecha: this.nuevaEvidencia.fecha,
+                resultadoObtenido: this.nuevaEvidencia.resultadoObtenido,
+                linkActaEvidencia: this.nuevaEvidencia.linkActaEvidencia,
+                metaId: this.nuevaEvidencia.metaId,
+            };
+
+            // Realizar una solicitud POST al servidor
+            fetch('https://localhost:7100/api/Evidenciums', {
+                method: 'POST',
+                headers: {
+                    "client-platform": "browser",
+                    'Content-Type': 'application/json',
+                    // Agregar cualquier otro encabezado necesario, como token de autenticación
+                },
+                body: JSON.stringify(nuevaEvidenciaData) // Convertir los datos a JSON
+            })
+                .then(response => response.json()) // Convertir la respuesta a JSON
+                .then(data => {
+
+                    // Aquí puedes manejar la respuesta del servidor después de crear el indicador
+                    console.log('Evidenciacreado:', data);
+                    console.log('Respuesta de la API:', data);
+
+                    // Redirigir a la lista de indicadores
+                    this.showModal = false;
+                    location.reload(); // Recarga la página
+
+                    // Puedes mostrar un mensaje de éxito o realizar otras acciones necesarias
+                })
+                .catch(error => {
+                    // Manejar cualquier error que ocurra durante la solicitud
+                    console.error('Error al agregar evidencia:', error);
+                    // Puedes mostrar un mensaje de error o realizar acciones de manejo de errores
+                });
+        },
+        actualizarMeta() {
+            // Preparar los datos actualizados del indicador
+            const updatedData = {
+                fecha: this.editarEvidencia.fecha,
+                resultadoObtenido: this.editarEvidencia.resultadoObtenido,
+                linkActaEvidencia: this.editarEvidencia.linkActaEvidencia,
+                metaId: this.editarEvidencia.metaId,
+            };
+
+            const itemId = this.editarEvidencia.id; // Asegúrate de tener la propiedad id en nuevaEvidencia para identificar el elemento a editar
+
+            // Realizar una solicitud PUT al servidor para actualizar
+            fetch(`https://localhost:7100/api/Evidenciums/${itemId}`, {
+                method: 'PUT',
+                headers: {
+                    "client-platform": "browser",
+                    'Content-Type': 'application/json',
+                    // Agregar cualquier otro encabezado necesario, como token de autenticación
+                },
+                body: JSON.stringify(updatedData),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Evidencia actualizada:', data);
+                    console.log('Respuesta de la API:', data);
+
+                    this.showModal2 = false;
+                    // Actualiza los datos en el componente en lugar de recargar la página
+                    this.actualizarDatos(updatedData); // Crea un método para actualizar los datos
+                })
+                .catch(error => {
+                    console.error('Error al actualizar la Evidencia:', error);
+                });
+        },
+        totalPages() {
+            const itemsPerPage = 5; // Cambia esto al número deseado de elementos por página
+            return Math.ceil(this.items.length / itemsPerPage);
+        },
+        setCurrentPage(page) {
+            const totalPages = this.totalPages();
+            this.currentPage = Math.min(Math.max(page, 1), totalPages);
+        },
+        saveNewItem() {
+            this.showModal = false;
+        },
+        editItem(item) {
+            // Aquí se debería manejar la lógica de edición
+            console.log(`Editar entidad con ID: ${item.id}`)
+        },
+        createNewItem() {
+            // Aquí se debería manejar la lógica de creación de nueva entidad
+
+        },
+        openEditModal(item) {
+            this.editarEvidencia = { ...item }; // Copia los datos del item en el formulario de edición
+            this.showModal2 = true; // Abre el modal de edición
+        },
+        actualizarDatos(updatedItem) {
+            // Busca el índice del elemento actualizado en la lista de items
+            const index = this.items.findIndex(item => item.id === updatedItem.id);
+            if (index !== -1) {
+                // Actualiza el elemento en la lista con los nuevos datos
+                this.items.splice(index, 1, updatedItem);
+            }
+        },
+    },
+    name: 'NuxtTableEntity',
+    computed: {
+        itemsForCurrentPage() {
+            const itemsPerPage = 5; // Cambia esto al número deseado de elementos por página
+            const startIndex = (this.currentPage - 1) * itemsPerPage;
+            const endIndex = startIndex + itemsPerPage;
+            return this.items.slice(startIndex, endIndex);
+        }
+    },
+    totalPages() {
+        const itemsPerPage = 5; // Cambia esto al número deseado de elementos por página
+        const totalItems = this.items.length;
+        return Math.ceil(totalItems / itemsPerPage);
+    },
+}
+</script>
--- pages/public/Meta/meta.vue
+++ pages/public/Meta/meta.vue
@@ -1,13 +1,59 @@
 <template>
-  <div>
-    <Menu/>
-    <h1>Meta</h1>
-  </div>
+  <main id="meta">
+      <header>
+          <Menu />
+      </header>
+      
+      <section class="container px-4 sm:px-8">
+          <div class="py-4">
+              <b-navbar-brand class="text-indigo-500 hover:text-indigo-600 inline-flex align-center gap-x-4" href="/">
+                <ReturnIcon />
+                <h1 class="text-lg font-bold">Meta</h1>
+              </b-navbar-brand>
+            </div>
+            <h1 class="italic text-sm subpixel-antialiased text-gray-400">Meta</h1>
+          <Meta :items="entities"/>
+      </section>
+      
+
+  </main>
+  
 </template>
-
 <script>
-export default {
-  name: 'NuxtMeta',
-}
 
+import Meta from '~/components/Meta.vue';
+import icons from '~/icons';
+
+
+export default {
+components: {
+  Meta,
+
+  ReturnIcon: icons.return
+},
+mounted() {
+  this.listServices();
+},
+methods: {
+  async listServices() {
+      const token = "Bearer eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYnlzb2RldiIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6ImJhc29sb3J6YW5vMUBlc3BlLmVkdS5lYyIsImV4cCI6MTY5Mjg1MjM2MH0.6JQD7P69m-WcSa7mYxqUSexiRr9l1qgBFeAm3N0uisQ"; 
+
+      const response = await fetch('https://localhost:7100/api/Metums', {
+        method: "GET",
+        headers: {
+          "client-platform": "browser",
+          "Content-Type": "application/json",
+          "Authorization": token
+        }
+      });
+      const data = await response.json()
+      this.entities = data;
+  }
+},
+data(){
+  return{
+    entities:[]
+  };
+  }
+};
 </script>
